0bbfe4d0fa9fe23a940579dd0d14a512dced057c
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 20:44:48 +0530

    Modularized TabTitle

---
+++ src/components/app-components/TableComponents/TabTitle.tsx
@@ -0,0 +1,9 @@
+import { TabProps } from "./interface/types";
+
+export default function TabTitle({ name }: Pick<TabProps, "name">) {
+    return (
+        <div className="">
+            <h2 className="text-base font-semibold">{name}</h2>
+        </div>
+    );
+}
