623e16dd5c810efa0dca8d705a130d7d5475df93
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 20:16:41 +0530

    Refactoring Tab code to work with updated type interfaces

--- src/components/app-components/TableComponents/Tab.tsx
+++ src/components/app-components/TableComponents/Tab.tsx
@@ -1,12 +1,13 @@
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import TabHeader from "./TabHeader";
 import TabRow from "./TabRow";
+import { TabProps } from "./interface/types";
 
-export default function Tab({ props }: { props: any }) {
+export default function Tab({ name, number }: TabProps) {
     return (
-        <div>
-            <TabHeader props={props} />
-            <Table className="border-r border-border">
+        <div className="border border-border rounded-md">
+            <TabHeader name={name} number={number} />
+            <Table className="border border-border">
                 {/* <TableHeader>
                 <TableRow>
                     <TableHead>Header 1</TableHead>
@@ -15,7 +16,7 @@ export default function Tab({ props }: { props: any }) {
             </TableHeader> */}
                 {/* No table header for now */}
                 <TableBody>
-                    <TabRow props={props} />
+                    {/* <TabRow props={props} /> */}
                 </TableBody>
             </Table>
         </div>
