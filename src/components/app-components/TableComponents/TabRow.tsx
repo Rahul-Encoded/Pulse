0de80d630f503c0d7ea11a5dec60a9b9b56d2c09
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 17:27:34 +0530

    Minimal Table row component added

---
+++ src/components/app-components/TableComponents/TabRow.tsx
@@ -0,0 +1,10 @@
+import { TableCell, TableRow } from "@/components/ui/table";
+
+export default function TabRow({ props }: { props: any }) {
+    return (
+        <TableRow>
+            <TableCell>Cell 1</TableCell>
+            <TableCell>Cell 2</TableCell>
+        </TableRow>
+    )
+}
