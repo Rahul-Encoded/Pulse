a44311db5d56cbd2002b52efbd2fcb7ea4db29ae
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 20:17:19 +0530

    Table enclosing New Pairs, Final Stretch and Migrated subtables have been refactored to be closer to pulse UI

--- src/components/app-components/TokenTable.tsx
+++ src/components/app-components/TokenTable.tsx
@@ -1,11 +1,15 @@
 import Tab from "./TableComponents/Tab";
+import { TabProps } from "./TableComponents/interface/types";
 
-export default function TokenTable({ props }: { props: any }) {
+const names = ["New Pairs", "Final Stretch", "Migrated"];
+
+
+export default function TokenTable() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <Tab props={props}></Tab>
-            <Tab props={props}></Tab>
-            <Tab props={props}></Tab>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0">
+            {names.map((name, index) => (
+                <Tab key={index} name={name} />
+            ))}
         </div>
     )
 }
