8d96ead7d1728778883b3e6bc0902ce1bd7948a4
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 20:46:09 +0530

    Modularized Tab Filter

---
+++ src/components/app-components/TableComponents/TabFilterAction.tsx
@@ -0,0 +1,9 @@
+import { SlidersHorizontal } from "lucide-react";
+
+export default function TabFilterAction() {
+    return (
+        <button className="p-2 hover:bg-secondary rounded-md transition-colors">
+            <SlidersHorizontal className="h-4 w-4" />
+        </button>
+    );
+}
