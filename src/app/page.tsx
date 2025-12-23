1a1f3265ba8dfdcd3d92e44f3f72b86ce6c70a60
Rahul-Encoded <rahulm03@outlook.com>
2025-12-23 20:18:02 +0530

    UI change for the main app page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import TokenTable from "@/components/app-components/TokenTable";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main>
-        <TokenTable props></TokenTable>
-      </main>
+    <div className="p-4">
+      <div className="w-full">
+        <TokenTable />
+      </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
       </footer>
