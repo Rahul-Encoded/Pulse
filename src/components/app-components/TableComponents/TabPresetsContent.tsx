cb4c84f1a4a531de0450e1369b05af145f7ba357
Rahul-Encoded <rahulm03@outlook.com>
2025-12-24 23:54:08 +0530

    using redux store values

--- src/components/app-components/TableComponents/TabPresetsContent.tsx
+++ src/components/app-components/TableComponents/TabPresetsContent.tsx
@@ -1,16 +1,19 @@
+"use client";
 import {
-    MenubarContent,
-    MenubarItem,
-    MenubarSeparator,
-} from "@/components/ui/menubar"
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+} from "@/components/ui/menubar";
+import { useAppSelector } from "@/lib/hooks";
 
 export default function TabPresetsContent() {
-    return (
-        <MenubarContent className="min-w-[4rem]">
-            <MenubarItem className="text-xs py-1 px-2">20%</MenubarItem>
-            <MenubarItem className="text-xs py-1 px-2">0.001</MenubarItem>
-            <MenubarItem className="text-xs py-1 px-2">0.01</MenubarItem>
-            <MenubarItem className="text-xs py-1 px-2">off</MenubarItem>
-        </MenubarContent>
-    );
+  const { slippage, priority, bribe } = useAppSelector((state) => state.trade);
+  return (
+    <MenubarContent className="min-w-[4rem]">
+      <MenubarItem className="text-xs py-1 px-2">{slippage}%</MenubarItem>
+      <MenubarItem className="text-xs py-1 px-2">{priority}</MenubarItem>
+      <MenubarItem className="text-xs py-1 px-2">{bribe}</MenubarItem>
+      <MenubarItem className="text-xs py-1 px-2">off</MenubarItem>
+    </MenubarContent>
+  );
 }
