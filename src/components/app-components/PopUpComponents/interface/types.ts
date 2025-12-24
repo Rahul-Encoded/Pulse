6aa20f456d08aa9206c4a59e141b663790ba8e6b
Rahul-Encoded <rahulm03@outlook.com>
2025-12-24 20:43:37 +0530

    Further modularization of TradingSettingsPopUp component

--- src/components/app-components/PopUpComponents/interface/types.ts
+++ src/components/app-components/PopUpComponents/interface/types.ts
@@ -1,4 +1,9 @@
-export default interface PopUpHeaderProps {
+export interface PopUpHeaderProps {
     setIsPopUpOpen: (open: boolean) => void;
     title: string;
+}
+
+export interface TradingSettingsPopUpProps {
+    isPopUpOpen: boolean;
+    setIsPopUpOpen: (open: boolean) => void;
 }
