ae3e9dee98fe3d0b747259295ebb7ac6ac53801e
Rahul-Encoded <rahulm03@outlook.com>
2025-12-24 17:18:44 +0530

    Refactored and updated PresetPopUp Component

--- src/components/app-components/PopUpComponents/PresetPopup.tsx
+++ src/components/app-components/PopUpComponents/PresetPopup.tsx
@@ -1,39 +1,38 @@
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { useState } from "react";
-import { cn } from "@/lib/utils";
-import TradeParameterInput from "./TradeParameterInput";
 
+import TradeParameterInput from "./TradeParameterInput";
+import AutoFee from "./AutoFee";
+import MaxFee from "./MaxFee";
+import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupText } from "@/components/ui/input-group";
+import BuyOrSellTab from "./BuyOrSellTab";
+import MevMode from "./MevMode";
+import Seperator from "../ComonComponents/Seperator";
 
 export default function PresetPopup() {
-    const [buySettings, setBuySettings] = useState<boolean>(true);
-    const [sellSettings, setSellSettings] = useState<boolean>(false);
-
-    const handleBuySettings = () => {
-        setBuySettings(!buySettings);
-        setSellSettings(false);
-    }
-
-    const handleSellSettings = () => {
-        setSellSettings(!sellSettings);
-        setBuySettings(false);
-    }
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4 justify-center items-center text-xs">
 
-            <Separator></Separator>
-
-            <Tabs defaultValue="account" className="w-[400px] p-4">
-                <TabsList className="w-full">
-                    <TabsTrigger value="Buy" onClick={handleBuySettings} className={cn(buySettings ? "data-[state=active]:bg-emerald-400/20 data-[state=active]:text-emerald-400" : "", "w-full")}>Buy Settings</TabsTrigger>
-                    <TabsTrigger value="Sell" onClick={handleSellSettings} className={cn(sellSettings ? "data-[state=active]:bg-red-400/20 data-[state=active]:text-red-400" : "", "w-full")}>Sell Settings</TabsTrigger>
-                </TabsList>
-
-            </Tabs>
+            <BuyOrSellTab></BuyOrSellTab>
 
             <TradeParameterInput />
+
+            <div className="flex justify-between items-center w-full">
+                <AutoFee></AutoFee>
+                <MaxFee></MaxFee>
+            </div>
+
+            <MevMode></MevMode>
+            <InputGroup className="rounded-full">
+                <InputGroupInput className="!pl-1" placeholder="https://a...e.com"></InputGroupInput>
+                <InputGroupAddon>
+                    <InputGroupText>RPC</InputGroupText>
+                </InputGroupAddon>
+            </InputGroup>
+
+            <Seperator className="w-[calc(100%+3rem)] -mx-6 my-2"></Seperator>
+
+            <Button className="rounded-full bg-blue-700 text-background w-full">Continue</Button>
 
         </div>
     );
