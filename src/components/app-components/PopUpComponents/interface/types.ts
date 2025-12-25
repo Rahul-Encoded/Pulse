b5c567745f8f346757994ab1b2f78aff52af8d23
Rahul-Encoded <rahulm03@outlook.com>
2025-12-25 15:59:59 +0530

    Updated TradingSettingsPopUpProps type

--- src/components/app-components/PopUpComponents/interface/types.ts
+++ src/components/app-components/PopUpComponents/interface/types.ts
@@ -1,9 +1,10 @@
 export interface PopUpHeaderProps {
-    setIsPopUpOpen: (open: boolean) => void;
-    title: string;
+  setIsPopUpOpen: (open: boolean) => void;
+  title: string;
 }
 
 export interface TradingSettingsPopUpProps {
-    isPopUpOpen: boolean;
-    setIsPopUpOpen: (open: boolean) => void;
+  presetId: string;
+  isPopUpOpen: boolean;
+  setIsPopUpOpen: (open: boolean) => void;
 }
