6986de01c36f8e5e5d3318c6d8fb9380905641e1
Rahul-Encoded <rahulm03@outlook.com>
2025-12-25 13:55:55 +0530

    added mevModeReducer in the redux store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,16 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tradeReducer from './features/trade/tradeSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import tradeReducer from "./features/trade/tradeSlice";
+import mevModeReducer from "./features/trade/mevModeSlice";
 
 export const makeStore = () => {
-    return configureStore({
-        reducer: {
-            trade: tradeReducer,
-        }
-    })
-}
+  return configureStore({
+    reducer: {
+      trade: tradeReducer,
+      mevMode: mevModeReducer,
+    },
+  });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
