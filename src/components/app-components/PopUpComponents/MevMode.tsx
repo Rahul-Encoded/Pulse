e1e5971ff6f5f0903024c032f37eb8ae3c6b30fd
Rahul-Encoded <rahulm03@outlook.com>
2025-12-25 13:58:17 +0530

    code to set mevMode store

--- src/components/app-components/PopUpComponents/MevMode.tsx
+++ src/components/app-components/PopUpComponents/MevMode.tsx
@@ -1,34 +1,77 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Info } from "lucide-react"
-import { useState } from "react"
-import { cn } from "@/lib/utils"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Info } from "lucide-react";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { MevModeState } from "@/lib/interface/types";
 
 const mevModes = ["Off", "Reduced", "Secure"];
-const mevModeImages = ["/shield-slash.svg", "/shield.svg", "/shield-double.svg"];
-const mevModeImagesSelected = ["/shield-slash-blue.svg", "/shield-blue.svg", "/shield-double-blue.svg"];
+const mevModeImages = [
+  "/shield-slash.svg",
+  "/shield.svg",
+  "/shield-double.svg",
+];
+const mevModeImagesSelected = [
+  "/shield-slash-blue.svg",
+  "/shield-blue.svg",
+  "/shield-double-blue.svg",
+];
 
+export default function MevMode({
+  mevMode,
+  setMevMode,
+}: {
+  mevMode: MevModeState;
+  setMevMode: (value: MevModeState) => void;
+}) {
+  return (
+    <div className="flex justify-between items-center w-full">
+      <div className="flex justify-around items-center">
+        <span className="pr-2 text-xs">MEV Mode</span>
+        <Info className="h-4 w-4 text-muted-foreground" />
+      </div>
 
-export default function MevMode() {
-    const [mevMode, setMevMode] = useState("Off");
-    return (
-        <div className="flex justify-between items-center w-full">
-            <div className="flex justify-around items-center">
-                <span className="pr-2 text-xs">MEV Mode</span>
-                <Info className="h-4 w-4 text-muted-foreground" />
-            </div>
-
-            <Tabs defaultValue="Off" className="">
-                <TabsList className="w-full bg-background border border-border">
-                    {mevModes.map((mode, index) => (
-                        <TabsTrigger value={mode} onClick={() => setMevMode(mode)} className={cn(mevMode === mode ? "data-[state=active]:bg-blue-400/20 data-[state=active]:text-blue-400" : "", "w-full text-xs")}>
-                            <Image src={mevModeImages[index]} alt={mode} width={12} height={12} className={cn("mr-[1px]", mevMode === mode ? "hidden" : "block")} />
-                            <Image src={mevModeImagesSelected[index]} alt={mode} width={12} height={12} className={cn("mr-[1px]", mevMode === mode ? "block" : "hidden")} />
-                            {mode}
-                        </TabsTrigger>
-                    ))}
-                </TabsList>
-            </Tabs>
-        </div>
-    )
+      <Tabs
+        value={mevMode.mevMode}
+        onValueChange={(value) => setMevMode({ mevMode: value })}
+        className=""
+      >
+        <TabsList className="w-full bg-background border border-border">
+          {mevModes.map((mode, index) => (
+            <TabsTrigger
+              key={mode}
+              value={mode}
+              className={cn(
+                mevMode.mevMode === mode
+                  ? "data-[state=active]:bg-blue-400/20 data-[state=active]:text-blue-400"
+                  : "",
+                "w-full text-xs"
+              )}
+            >
+              <Image
+                src={mevModeImages[index]}
+                alt={mode}
+                width={12}
+                height={12}
+                className={cn(
+                  "mr-[1px]",
+                  mevMode.mevMode === mode ? "hidden" : "block"
+                )}
+              />
+              <Image
+                src={mevModeImagesSelected[index]}
+                alt={mode}
+                width={12}
+                height={12}
+                className={cn(
+                  "mr-[1px]",
+                  mevMode.mevMode === mode ? "block" : "hidden"
+                )}
+              />
+              {mode}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+    </div>
+  );
 }
