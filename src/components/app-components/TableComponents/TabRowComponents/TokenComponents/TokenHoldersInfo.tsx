604b160a189c8d06dc88153ebdfa241a6a70e50d
Rahul-Encoded <rahulm03@outlook.com>
2025-12-27 05:11:13 +0530

    Added TokenHoldersInfo Component

---
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TokenHoldersInfo.tsx
@@ -0,0 +1,57 @@
+import { Token } from "@/lib/interface/tokens";
+import Image from "next/image";
+import TokenCapItem from "./TokenCapItem";
+
+export default function TokenHoldersInfo({ token }: { token: Token }) {
+  const mainItems = [
+    {
+      value: `${token.holdersCount}`,
+      icon: <Image src="/users.svg" alt="Holders" width={16} height={16} />,
+      tooltip: "Holders",
+    },
+    {
+      value: `${token.proTradersCount}`,
+      icon: (
+        <Image src="/candle.svg" alt="Pro Traders" width={16} height={16} />
+      ),
+      tooltip: "Pro Traders",
+    },
+    {
+      value: `${token.kolsCount}`,
+      icon: <Image src="/cup.svg" alt="KOLs" width={16} height={16} />,
+      tooltip: "KOLs",
+    },
+    {
+      value: `${token.devMigrationsCount}`,
+      icon: (
+        <Image
+          src="/crown.svg"
+          alt="Dev Migrations/Created"
+          width={16}
+          height={16}
+        />
+      ),
+      tooltip: "Dev Migrations/Created",
+    },
+    {
+      value: `${token.bundleHolding}`,
+      icon: (
+        <Image
+          src="/cubes-green.svg"
+          alt="Bundle Holding"
+          width={16}
+          height={16}
+        />
+      ),
+      tooltip: "Bundle Holding",
+    },
+  ];
+
+  return (
+    <div className="flex items-center gap-2">
+      {mainItems.map((item) => (
+        <TokenCapItem key={item.value} {...item} className="text-xs" />
+      ))}
+    </div>
+  );
+}
