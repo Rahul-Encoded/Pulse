5c59746ecf237691732d9f6c220f9c3d65cf7fdf
Rahul-Encoded <rahulm03@outlook.com>
2025-12-27 04:25:01 +0530

    Necessary interface types for TokenCapItem and SocialIcon props

---
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/interface/types.ts
@@ -0,0 +1,19 @@
+export interface TokenCapItemProps {
+  label: string;
+  value: string | number;
+  icon?: React.ReactNode;
+  extra?: React.ReactNode;
+  tooltip: string;
+  className?: string;
+}
+
+export interface SocialIconProps
+  extends Omit<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    "width" | "height" | "src" | "alt"
+  > {
+  src: string;
+  hoverSrc: string;
+  alt: string;
+  link: string;
+}
