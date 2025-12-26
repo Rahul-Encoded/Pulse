08b4edef608e3ae364814c26f1507c3e5c10ceed
Rahul-Encoded <rahulm03@outlook.com>
2025-12-27 04:22:09 +0530

    Component for Market Cap and other related values

---
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TokenCaps.tsx
@@ -0,0 +1,50 @@
+import { Token } from "@/lib/interface/tokens";
+import { SentimentBar } from "./SentimentBar";
+import Image from "next/image";
+import TokenCapItem from "./TokenCapElements";
+
+export default function TokenCaps({ token }: { token: Token }) {
+  const mainItems = [
+    {
+      label: "MC",
+      value: `$${(token.marketCap / 1000).toFixed(2)}K`,
+      tooltip: "Market Value",
+      className: `text-${token.mcColor}`,
+    },
+    {
+      label: "V",
+      value: `$${(token.volume / 1000).toFixed(0)}K`,
+      tooltip: "Volume",
+    },
+  ];
+
+  const secondaryItems = [
+    {
+      label: "F",
+      icon: (
+        <Image src="/solanaLogoMark.svg" alt="solana" width={10} height={10} />
+      ),
+      value: token.globalFees,
+      tooltip: "Fees",
+    },
+    {
+      label: "TX",
+      value: token.transactions.buys + token.transactions.sells,
+      extra: <SentimentBar token={token} />,
+      tooltip: "Transactions",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-1">
+      {mainItems.map((item) => (
+        <TokenCapItem key={item.label} {...item} />
+      ))}
+      <div className="flex items-center gap-4">
+        {secondaryItems.map((item) => (
+          <TokenCapItem key={item.label} {...item} className="text-xs" />
+        ))}
+      </div>
+    </div>
+  );
+}
