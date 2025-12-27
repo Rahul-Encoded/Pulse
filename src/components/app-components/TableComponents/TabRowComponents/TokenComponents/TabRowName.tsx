bcf7980c5748e98e44dd5943dd3508b1b8e598ed
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 01:10:06 +0530

    Added Tooltip to the Token Symbol

--- src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TabRowName.tsx
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TabRowName.tsx
@@ -4,33 +4,42 @@ import { Button } from "@/components/ui/button";
 import { Token } from "@/lib/interface/tokens";
 import { Copy, Clipboard } from "lucide-react";
 import { toast } from "sonner";
+import { GenericTooltip } from "../Tooltip/GenericTooltip";
 
 export default function TabRowName({ token }: { token: Token }) {
   return (
     <div className="flex items-center gap-2">
       <h2 className="text-base font-medium">{token.name}</h2>
-      <Button
-        className="p-0 bg-transparent hover:bg-transparent border-none flex items-center gap-1.5 group transition-colors"
-        onClick={(e) => {
-          e.stopPropagation();
-          navigator.clipboard.writeText(token.address);
-          toast(
-            <div className="flex items-center gap-2">
-              <Clipboard className="text-blue-600" size={16} />
-              <span>Address copied to clipboard</span>
-            </div>
-          );
-        }}
-      >
-        <h2 className="text-base font-medium text-foreground/60 group-hover:text-blue-600 transition-colors">
-          {token.symbol}
-        </h2>
+      <GenericTooltip
+        trigger={
+          <Button
+            className="p-0 bg-transparent hover:bg-transparent border-none flex items-center gap-1.5 group transition-colors"
+            onClick={(e) => {
+              e.stopPropagation();
+              navigator.clipboard.writeText(token.address);
+              toast(
+                <div className="flex items-center gap-2">
+                  <Clipboard className="text-blue-600" size={16} />
+                  <span>Address copied to clipboard</span>
+                </div>
+              );
+            }}
+          >
+            <h2 className="text-base font-medium text-foreground/60 group-hover:text-blue-600 transition-colors">
+              {token.symbol}
+            </h2>
 
-        <Copy
-          size={16}
-          className="text-foreground/60 group-hover:text-blue-600 transition-colors"
-        />
-      </Button>
+            <Copy
+              size={16}
+              className="text-foreground/60 group-hover:text-blue-600 transition-colors"
+            />
+          </Button>
+        }
+        content={
+          <span className="text-xs text-foreground/80">{token.symbol}</span>
+        }
+        side="top"
+      ></GenericTooltip>
     </div>
   );
 }
