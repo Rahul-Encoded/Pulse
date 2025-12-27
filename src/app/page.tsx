fffad0990417619c9922342cd9b1e498fef5572b
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 05:24:31 +0530

    Added NavBar component to the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/app-components/ComonComponents/Navbar";
 import TokenTable from "@/components/app-components/TokenTable";
 
 export default function Home() {
   return (
     <div className="p-4">
       <div className="w-full">
+        <Navbar />
         <TokenTable />
       </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
