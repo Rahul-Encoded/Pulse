11810906d9ad01340f36c783b3c046dc5ed83482
Rahul-Encoded <rahulm03@outlook.com>
2025-12-27 06:11:08 +0530

    created TokenBadges Component

---
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TokenBadges.tsx
@@ -0,0 +1,96 @@
+import { Token } from "@/lib/interface/tokens";
+import { GenericTooltip } from "../Tooltip/GenericTooltip";
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+
+export default function TokenBadges({ token }: { token: Token }) {
+  const mainItems = [
+    {
+      extra: (
+        <Badge variant="outline" className="text-red-400 px-1 rounded-full">
+          <Image
+            src="/user-star-red.svg"
+            alt="Bubble Map"
+            width={16}
+            height={16}
+            className="border-none"
+          />
+          {token.bubbleMapPercentage}%
+        </Badge>
+      ),
+      tooltip: "Bubble Map",
+    },
+    {
+      extra: (
+        <Badge variant="outline" className="text-red-400 px-1 rounded-full">
+          <Image
+            src="/sniper-red.svg"
+            alt="Sniper Holding"
+            width={16}
+            height={16}
+            className="border-none"
+          />
+          {token.sniperHolding}%
+        </Badge>
+      ),
+      tooltip: "Sniper Holding",
+    },
+    {
+      extra: (
+        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
+          <Image
+            src="/mole-green.svg"
+            alt="Insider Holding"
+            width={16}
+            height={16}
+            className="border-none"
+          />
+          {token.insiderHolding}%
+        </Badge>
+      ),
+      tooltip: "Insider Holding",
+    },
+    {
+      extra: (
+        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
+          <Image
+            src="/cubes-green.svg"
+            alt="Bundle Holding"
+            width={16}
+            height={16}
+            className="border-none"
+          />
+          {token.bundleHolding}%
+        </Badge>
+      ),
+      tooltip: "Bundle Holding",
+    },
+    {
+      extra: token.isPaid ? (
+        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
+          <Image
+            src="/eagle.svg"
+            alt="Paid"
+            width={16}
+            height={16}
+            className="border-none"
+          />
+        </Badge>
+      ) : null,
+      tooltip: "Paid",
+    },
+  ];
+  return (
+    <div className="flex items-center gap-2">
+      {mainItems.map((item) => (
+        <GenericTooltip
+          key={item.tooltip}
+          trigger={item.extra}
+          content={<span>{item.tooltip}</span>}
+          contentClassName="text-xs bg-badges text-foreground"
+          side="top"
+        />
+      ))}
+    </div>
+  );
+}
