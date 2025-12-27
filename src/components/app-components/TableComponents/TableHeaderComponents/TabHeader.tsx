44c097894ce9981fca32f7b5273343f750ca1ce3
Rahul-Encoded <rahulm03@outlook.com>
2025-12-27 23:38:56 +0530

    Added sticky top-0 z-10

--- src/components/app-components/TableComponents/TableHeaderComponents/TabHeader.tsx
+++ src/components/app-components/TableComponents/TableHeaderComponents/TabHeader.tsx
@@ -6,7 +6,7 @@ import TabFilterAction from "./TabFilterAction";
 
 export default function TabHeader({ name, number }: TabProps) {
   return (
-    <div className="flex items-center justify-between p-4 bg-table h-12 border border-border">
+    <div className="flex items-center justify-between p-4 bg-table h-12 border border-border sticky top-0 z-10">
       <TabTitle name={name} />
 
       <div className="flex items-center gap-4">
