93f0431dd1a1f8053c44ed70ab9e6835773796e6
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 03:23:20 +0530

    Edited TabRow component to use React.forwardRef

--- src/components/app-components/TableComponents/TabRowComponents/TabRow.tsx
+++ src/components/app-components/TableComponents/TabRowComponents/TabRow.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Token } from "@/lib/interface/tokens";
-import { memo, useEffect, useRef, useState } from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import TokenMedia from "./TokenComponents/TokenMedia";
 import TabRowName from "./TokenComponents/TabRowName";
@@ -9,32 +9,43 @@ import TokenCaps from "./TokenComponents/TokenCaps";
 import TokenInfoGrid from "./TokenComponents/TokenInfoGrid";
 import TokenBadges from "./TokenComponents/TokenBadges";
 
-interface TabRowProps {
+interface TabRowProps extends React.HTMLAttributes<HTMLTableRowElement> {
   token: Token;
 }
 
-const TabRow = memo(({ token }: TabRowProps) => {
-  return (
-    <TableRow className="border-b border-border hover:bg-rowhover cursor-pointer transition-colors">
-      <TableCell className="p-0">
-        <div className="flex flex-row w-full gap-2 pl-2 pr-2 pt-0 pb-2 items-start">
-          <TokenMedia token={token} />
+const TabRow = memo(
+  React.forwardRef<HTMLTableRowElement, TabRowProps>(
+    ({ token, className, ...props }, ref) => {
+      return (
+        <TableRow
+          ref={ref}
+          className={cn(
+            "border-b border-border hover:bg-rowhover cursor-pointer transition-colors",
+            className
+          )}
+          {...props}
+        >
+          <TableCell className="p-0">
+            <div className="flex flex-row w-full gap-2 pl-2 pr-2 pt-0 pb-2 items-start">
+              <TokenMedia token={token} />
 
-          <div className="flex flex-col flex-1 gap-1">
-            <div className="flex flex-row justify-between items-start w-full">
-              <div className="flex flex-col gap-0">
-                <TabRowName token={token} />
-                <TokenInfoGrid token={token} />
+              <div className="flex flex-col flex-1 gap-1">
+                <div className="flex flex-row justify-between items-start w-full">
+                  <div className="flex flex-col gap-0">
+                    <TabRowName token={token} />
+                    <TokenInfoGrid token={token} />
+                  </div>
+                  <TokenCaps token={token} />
+                </div>
+                <TokenBadges token={token} />
               </div>
-              <TokenCaps token={token} />
             </div>
-            <TokenBadges token={token} />
-          </div>
-        </div>
-      </TableCell>
-    </TableRow>
-  );
-});
+          </TableCell>
+        </TableRow>
+      );
+    }
+  )
+);
 
 TabRow.displayName = "TabRow";
 export default TabRow;
