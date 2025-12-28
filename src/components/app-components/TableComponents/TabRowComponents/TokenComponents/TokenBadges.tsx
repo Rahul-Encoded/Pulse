805be038a13825e49aef43dd504d91d9c5490c36
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 05:45:32 +0530

    Started using Lucide-icons instead of svgs in TokenBadges. Also introduced randomness to get random colors for badges

--- src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TokenBadges.tsx
+++ src/components/app-components/TableComponents/TabRowComponents/TokenComponents/TokenBadges.tsx
@@ -1,89 +1,95 @@
+import React, { useMemo } from "react";
 import { Token } from "@/lib/interface/tokens";
 import { GenericTooltip } from "../Tooltip/GenericTooltip";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { CircleAlert, Zap } from "lucide-react";
+import {
+  Boxes,
+  CircleAlert,
+  Crosshair,
+  Ghost,
+  UserStar,
+  Zap,
+} from "lucide-react";
 import { toast } from "sonner";
 import Toaster from "@/components/app-components/ComonComponents/Toaster";
 
 export default function TokenBadges({ token }: { token: Token }) {
-  const mainItems = [
-    {
-      extra: (
-        <Badge variant="outline" className="text-red-400 px-1 rounded-full">
-          <Image
-            src="/user-star-red.svg"
-            alt="Bubble Map"
-            width={16}
-            height={16}
-            className="border-none"
-          />
-          {token.bubbleMapPercentage}%
-        </Badge>
-      ),
-      tooltip: "Bubble Map",
-    },
-    {
-      extra: (
-        <Badge variant="outline" className="text-red-400 px-1 rounded-full">
-          <Image
-            src="/sniper-red.svg"
-            alt="Sniper Holding"
-            width={16}
-            height={16}
-            className="border-none"
-          />
-          {token.sniperHolding}%
-        </Badge>
-      ),
-      tooltip: "Sniper Holding",
-    },
-    {
-      extra: (
-        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
-          <Image
-            src="/mole-green.svg"
-            alt="Insider Holding"
-            width={16}
-            height={16}
-            className="border-none"
-          />
-          {token.insiderHolding}%
-        </Badge>
-      ),
-      tooltip: "Insider Holding",
-    },
-    {
-      extra: (
-        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
-          <Image
-            src="/cubes-green.svg"
-            alt="Bundle Holding"
-            width={16}
-            height={16}
-            className="border-none"
-          />
-          {token.bundleHolding}%
-        </Badge>
-      ),
-      tooltip: "Bundle Holding",
-    },
-    {
-      extra: token.isPaid ? (
-        <Badge variant="outline" className="text-green-400 px-1 rounded-full">
-          <Image
-            src="/eagle.svg"
-            alt="Paid"
-            width={16}
-            height={16}
-            className="border-none"
-          />
-        </Badge>
-      ) : null,
-      tooltip: "Paid",
-    },
-  ];
+  const colors = ["text-red-400", "text-green-400"];
+  const getRandomColor = () =>
+    colors[Math.floor(Math.random() * colors.length)];
+
+  const mainItems = useMemo(
+    () => [
+      {
+        extra: (
+          <Badge
+            variant="outline"
+            className={`${getRandomColor()} px-1 rounded-full`}
+          >
+            <UserStar size={16} />
+            {token.bubbleMapPercentage}%
+          </Badge>
+        ),
+        tooltip: "Bubble Map",
+      },
+      {
+        extra: (
+          <Badge
+            variant="outline"
+            className={`${getRandomColor()} px-1 rounded-full`}
+          >
+            <Crosshair size={16} />
+            {token.sniperHolding}%
+          </Badge>
+        ),
+        tooltip: "Sniper Holding",
+      },
+      {
+        extra: (
+          <Badge
+            variant="outline"
+            className={`${getRandomColor()} px-1 rounded-full`}
+          >
+            <Ghost size={16} />
+            {token.insiderHolding}%
+          </Badge>
+        ),
+        tooltip: "Insider Holding",
+      },
+      {
+        extra: (
+          <Badge
+            variant="outline"
+            className={`${getRandomColor()} px-1 rounded-full`}
+          >
+            <Boxes size={16} />
+            {token.bundleHolding}%
+          </Badge>
+        ),
+        tooltip: "Bundle Holding",
+      },
+      {
+        extra: token.isPaid ? (
+          <Badge
+            variant="outline"
+            className={`${getRandomColor()} px-1 rounded-full`}
+          >
+            <Image
+              src="/eagle.svg"
+              alt="Paid"
+              width={16}
+              height={16}
+              className="border-none"
+            />
+          </Badge>
+        ) : null,
+        tooltip: "Paid",
+      },
+    ],
+    [token]
+  );
   return (
     <div className="flex justify-between w-full">
       <div className="flex items-center gap-2">
