c01445a666c3585cdc8014fcd8b9abea55da0469
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 18:06:13 +0530

    Wrapping layout with ToolTipProvider to avoid redundant rendering overhead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import StoreProvider from "./StoreProvider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,7 +40,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <TooltipProvider>{children}</TooltipProvider>
             <Toaster position="top-center" />
           </ThemeProvider>
         </StoreProvider>
