be23d581b9f9e33004922690257f86f4f56504f2
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 15:44:47 +0530

    Updated TokenTable to work with UiSlice redux variables

--- src/components/app-components/TokenTable.tsx
+++ src/components/app-components/TokenTable.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
+import { selectActiveTab } from "@/lib/features/ui/uiSlice";
 import Tab from "./TableComponents/Tab";
-import { TabProps } from "./TableComponents/interface/types";
 
 const names = ["New Pairs", "Final Stretch", "Migrated"];
 
 export default function TokenTable() {
+  const activeTab = useAppSelector(selectActiveTab);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[1px] bg-border border border-border rounded-lg overflow-hidden h-[calc(100vh-80px)]">
-      {names.map((name, index) => (
-        <Tab key={index} name={name} />
-      ))}
+    <div className="flex flex-col lg:grid lg:grid-cols-3 gap-[1px] bg-border border border-border rounded-lg overflow-hidden h-[calc(100vh-120px)] md:h-[calc(100vh-80px)]">
+      {names.map((name, index) => {
+        const isActive = activeTab === name;
+        return (
+          <div
+            key={index}
+            className={
+              isActive ? "flex-1 min-h-0" : "hidden lg:block flex-1 min-h-0"
+            }
+          >
+            <Tab name={name} />
+          </div>
+        );
+      })}
     </div>
   );
 }
