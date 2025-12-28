5f975025c591998a490afb47017fa69c8679ee97
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 18:12:54 +0530

    Removed ToolTipProvider import as we shifted it to layout

--- src/components/app-components/TableComponents/TabRowComponents/Tooltip/GenericTooltip.tsx
+++ src/components/app-components/TableComponents/TabRowComponents/Tooltip/GenericTooltip.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
@@ -19,19 +18,17 @@ export function GenericTooltip({
   align = "center",
 }: GenericTooltipProps) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild className={cn(triggerClassName)}>
-          {trigger}
-        </TooltipTrigger>
-        <TooltipContent
-          side={side}
-          align={align}
-          className={cn("bg-badges", contentClassName)}
-        >
-          {content}
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild className={cn(triggerClassName)}>
+        {trigger}
+      </TooltipTrigger>
+      <TooltipContent
+        side={side}
+        align={align}
+        className={cn("bg-badges", contentClassName)}
+      >
+        {content}
+      </TooltipContent>
+    </Tooltip>
   );
 }
