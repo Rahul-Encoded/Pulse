04708bf8544e5f27708521a99bdb28e246baa690
Rahul-Encoded <rahulm03@outlook.com>
2025-12-28 18:08:44 +0530

    Added tooltip for filter

--- src/components/app-components/TableComponents/TableHeaderComponents/TabFilterAction.tsx
+++ src/components/app-components/TableComponents/TableHeaderComponents/TabFilterAction.tsx
@@ -1,9 +1,16 @@
 import { SlidersHorizontal } from "lucide-react";
+import { GenericTooltip } from "../TabRowComponents/Tooltip/GenericTooltip";
 
 export default function TabFilterAction() {
   return (
-    <button className="p-2 hover:bg-secondary rounded-md transition-colors">
-      <SlidersHorizontal className="h-4 w-4" />
-    </button>
+    <GenericTooltip
+      trigger={
+        <div className="p-2 hover:bg-secondary rounded-md transition-colors cursor-pointer">
+          <SlidersHorizontal className="h-4 w-4" />
+        </div>
+      }
+      content={<span className="text-xs text-foreground">Filters</span>}
+      side="top"
+    />
   );
 }
